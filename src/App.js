7acbbc0c40d86d9b597151d390725e4130978ef0
Eliana Kern <Eliana Kern>
2022-02-07 18:07:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import LockIcon from '@mui/icons-material/Lock';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Link from '@mui/material/Link';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Box
+      component="form"
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        mt: 3,
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Avatar sx={{ bgcolor: '#6519F0', width: 45, height: 45 }}>
+        <LockIcon />
+      </Avatar>
+      <Typography variant="h5" sx={{ mb: 2 }}>
+        Sign up
+      </Typography>
+      <Box sx={{ display: 'flex' }}>
+        <TextField sx={{ m: 1 }} label="First Name *"></TextField>
+        <TextField sx={{ m: 1 }} label="Last Name *"></TextField>
+      </Box>
+      <Box>
+        <TextField fullWidth sx={{ m: 1 }} label="Email Address *"></TextField>
+        <TextField fullWidth sx={{ m: 1 }} label="Password *"></TextField>
+      </Box>
 
+      <CardActions
+        sx={{ width: 400, display: 'flex', flexDirection: 'column' }}
+      >
+        <FormControlLabel
+          control={
+            <Checkbox sx={{ mr: 1 }} value="allowExtraEmails" color="primary" />
+          }
+          label="I want to receive inspiration, marketing promotions and updates via email."
+        />
+        <Button fullWidth sx={{ mt: 2, p: 1 }} variant="contained">
+          SIGN UP
+        </Button>
+
+        <Link sx={{ mt: 2, ml: 22 }} href="#" variant="body2">
+          Already have an account? Sign in
+        </Link>
+
+        <Typography sx={{ color: 'text.secondary', mt: 5 }} variant="body2">
+          Copyright © <Link color="inherit">Your Website</Link> 2022.
+        </Typography>
+      </CardActions>
+    </Box>
+  );
+};
 export default App;
